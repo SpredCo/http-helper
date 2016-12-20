cb3671af52532f8407843d5194e8c5af927b0cf1
Maxime Guedj <maxime@guedjdev.com>
2016-12-20 20:21:38 +0100

    Add mail utils

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const lib = {
   utils: require('./lib/utils'),
   error: require('./lib/error'),
   requestLogger: require('./lib/logger'),
-  sendReply: require('./lib/utils').sendReply
+  sendReply: require('./lib/utils').sendReply,
+  sendMail: require('./lib/mailer')
 };
 
 module.exports = lib;
