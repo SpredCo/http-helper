8a96e40405a22a97f3887298b1580e56213ecf66
Maxime Guedj <maxime@guedjdev.com>
2016-12-22 18:35:24 +0100

    Update mail templates

--- testmail.js
+++ testmail.js
@@ -1,5 +1,5 @@
 const httpHelper = require('./index');
 
-httpHelper.sendMail('max.guedj13@gmail.com', 'test', {username: 'Guedj'}, function (err) {
+httpHelper.sendMail('max.guedj13@gmail.com', 'welcome', {username: 'Guedj'}, function (err) {
   console.log(err);
 });
